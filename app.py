8845d842d2c962ed90d7806b1365040055831174
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-11-18 11:44:23 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+list = [
+    "test1",
+    "test2",
+    "test3",
+    "test4",
+    "test5",
+    "test6",
+    "test7",
+]
+
+@app.route("/<name>")
+def hello(name):
+    return render_template("index.html", name=name, list=list)
+
+if __name__ == "__main__":
+    app.run()
