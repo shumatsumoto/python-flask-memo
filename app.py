488fcc912957cd5771a86905c9948e50cf0cd828
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-11-18 12:21:36 +0900

    top画面実装（ループ処理）

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from flask import render_template
 
 app = Flask(__name__)
 
+memo_list = [
+    {"title": "test01", "body": "Shuです。"},
+    {"title": "test02", "body": "ちいかわです。"},
+    {"title": "test03", "body": "ひよこです。"},
+]
+
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    return render_template("index.html", memo_list=memo_list)
 
 if __name__ == "__main__":
     app.run()
