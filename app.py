ffdd4658cbe1db13df559d49353a6159a7e2eb2b
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-11-20 10:29:21 +0900

    DB接続

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, g
+import sqlite3
+DATABASE="flaskmemo.db"
 
 app = Flask(__name__)
 
-memo_list = [
-    {"title": "test01", "body": "Shuです。"},
-    {"title": "test02", "body": "ちいかわです。"},
-    {"title": "test03", "body": "ひよこです。"},
-]
-
 @app.route("/")
-def hello():
+def top():
+    memo_list = get_db().execute("select id, title, body from memo").fetchall()
     return render_template("index.html", memo_list=memo_list)
 
 if __name__ == "__main__":
     app.run()
+
+#database
+def connect_db():
+    rv = sqlite3.connect(DATABASE)
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    if not hasattr(g, "sqlite_db"):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
